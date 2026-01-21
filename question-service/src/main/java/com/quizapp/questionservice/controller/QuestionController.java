ab271ed3b4d25c6973909b2a98e34b6b1e320b0d
Archan47 <chowdhuryarchan76@gmail.com>
2026-01-21 23:53:48 +0530

    started question-service

---
+++ question-service/src/main/java/com/quizapp/questionservice/controller/QuestionController.java
@@ -0,0 +1,37 @@
+package com.quizapp.questionservice.controller;
+
+import com.quizapp.questionservice.entity.Question;
+import com.quizapp.questionservice.services.QuestionService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/question")
+public class QuestionController {
+
+    @Autowired
+    QuestionService questionService;
+
+    @GetMapping("/allQuestions")
+    public ResponseEntity <List<Question>> getAllQuestions(){
+        return questionService.getAllQuestions();
+    }
+
+    @GetMapping("/allQuestions/{category}")
+    public ResponseEntity <List<Question>> getQuestionsByCategory(@PathVariable String category){
+        return questionService.getQuestionsByCategory(category);
+    }
+
+    @GetMapping("/allQuestions/{category}/{difficultyLevel}")
+    public ResponseEntity <List<Question>> getQuestionsByLevel(@PathVariable String category ,@PathVariable String difficultyLevel){
+        return questionService.getLevelWiseQuestions(category,difficultyLevel);
+    }
+
+
+}
---
+++ question-service/src/main/java/com/quizapp/questionservice/services/QuestionService.java
@@ -0,0 +1,70 @@
+package com.quizapp.questionservice.services;
+
+
+import com.quizapp.questionservice.dao.QuestionDao;
+import com.quizapp.questionservice.entity.Question;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+@Service
+public class QuestionService {
+
+    @Autowired
+    QuestionDao questionDao;
+
+    public ResponseEntity< List<Question>> getAllQuestions() {
+        try{
+            return new ResponseEntity<>( questionDao.findAll(), HttpStatus.OK);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return new ResponseEntity<>(new ArrayList<>(),HttpStatus.BAD_REQUEST);
+
+    }
+
+    // Topic categorization
+    public ResponseEntity <List<Question>> getQuestionsByCategory(String category) {
+        try{
+            return new ResponseEntity<>(questionDao.findByCategory(category),HttpStatus.OK);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return new ResponseEntity<>(new ArrayList<>(),HttpStatus.BAD_REQUEST);
+    }
+
+    // Level categorization
+    public ResponseEntity <List<Question>> getLevelWiseQuestions(String category,String difficultyLevel) {
+        try{
+        return new ResponseEntity<>(questionDao.findByCategoryAndDifficultyLevel(category,difficultyLevel),HttpStatus.OK);
+        } catch (RuntimeException e) {
+            e.printStackTrace();
+        }
+        return new ResponseEntity<>(new ArrayList<>(),HttpStatus.BAD_REQUEST);
+    }
+
+    // add question (Admin)
+    public String addQuestion(Question question) {
+        questionDao.save(question);
+        return "success";
+    }
+
+    // update question (Admin)
+    public String updateQuestion(Question question) {
+        questionDao.save(question);
+        return "updated question";
+    }
+
+    // delete question (Admin)
+    public void deleteQuestion(Integer id) {
+        if (!questionDao.existsById(id)) {
+            throw new RuntimeException("Question not found with id: " + id);
+        }
+        questionDao.deleteById(id);
+    }
+}
